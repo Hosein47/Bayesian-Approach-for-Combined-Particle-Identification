3244c07e9d5a631e69ec080ddf7bbf5397632e11
Hosein Hashemi <hosein.hashemi@protonmail.ch>
2020-04-03 08:14:04 +0200

    Add files via upload

---
+++ multi_class/utils.py
@@ -0,0 +1,38 @@
+from typing import Union, Callable, List
+
+import numpy as np
+import tensorflow as tf
+
+
+Number = Union[
+    float,
+    int,
+    np.float16,
+    np.float32,
+    np.float64,
+    np.int8,
+    np.int16,
+    np.int32,
+    np.int64,
+    np.uint8,
+    np.uint16,
+    np.uint32,
+    np.uint64,
+]
+
+Initializer = Union[None, dict, str, Callable]
+Regularizer = Union[None, dict, str, Callable]
+Constraint = Union[None, dict, str, Callable]
+Activation = Union[None, str, Callable]
+
+TensorLike = Union[
+    List[Union[Number, list]],
+    tuple,
+    Number,
+    np.ndarray,
+    tf.Tensor,
+    tf.SparseTensor,
+    tf.Variable,
+]
+FloatTensorLike = Union[tf.Tensor, float, np.float16, np.float32, np.float64]
+AcceptableDTypes = Union[tf.DType, np.dtype, type, int, str, None]
